09b98454b860a67c3cc548f3a3afb2b353a91d25
Poleshook <poleshook@mail.ru>
2024-09-18 22:25:17 +0500

    module5hard.py

--- module5hard.py
+++ module5hard.py
@@ -2,18 +2,33 @@ import time
 
 
 class User:
-    def __init__(self, nickname: str, password: int, age: int):
+    def __init__(self, nickname, password, age):
         self.nickname = nickname
         self.password = hash(password)
         self.age = age
 
+    def __str__(self):
+        return self.nickname
+
+    def __eq__(self, other):
+        return other.nickname == self.nickname
+
+    def get_info(self):
+        return self.nickname, self.password
+
 
 class Video:
-    def __init__(self, title: str, duration: int, adult_mode: bool = False):
+    def __init__(self, title, duration, adult_mode=False):
         self.title = title
         self.duration = duration
         self.time_now = 0
         self.adult_mode = adult_mode
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return self.title
 
 
 class UrTube:
@@ -24,51 +39,50 @@ class UrTube:
 
     def log_in(self, nickname, password):
         for user in self.users:
-            if user.nickname == nickname and user.password == hash(password):
+            if (nickname, hash(password)) == user.get_info():
                 self.current_user = user
+                return user
 
-    def register(self, nickname: str, password: int, age: int):
-        password = hash(password)
-        for user in self.users:
-            if user.nickname == nickname:
-                print(f"Пользователь {nickname} уже существует")
-                return
+    def register(self, nickname, password, age):
+        new_user = User(nickname, password, age)
+        if new_user not in self.users:
+            self.users.append(new_user)
+            self.current_user = new_user
         else:
-            user_2 = User(nickname, password, age)
-            self.users.append(user_2)
-            print(f"{nickname} выполнен вход")
+            print(f'Пользователь {nickname} уже существует')
 
     def log_out(self):
         self.current_user = None
 
-    def add(self, *args):
-        for video_clip in args:
-            if not any (i.title == video_clip.title for i in self.videos):
-                self.videos.append(video_clip)
-            else:
-                print("Видео с таким названием уже существует")
+    def add(self, *videos: Video):
+        for video in videos:
+            if video.title not in self.videos:
+                self.videos.append(video)
 
     def get_videos(self, search):
-        search2 = search.upper()
-        return [video_clip.title for video_clip in self.videos if search2 in video_clip.title.upper()]
+        titles = []
+        for video in self.videos:
+            if search.lower() in str(video).lower():
+                titles.append(video)
+
+        return titles
 
     def watch_video(self, title):
-        if not self.current_user:
-            print("Зарегистрируйтесь или авторизуйтесь для просмотра видео")
+        if self.current_user is None:
+            print('Войти в аккаунт чтобы смотреть видео')
             return
-
-        for i in self.videos:
-            if i.title == title:
-                if i.adult_mode and self.current_user.age < 18:
-                    print("Доступ к этому видео запрещён. Вам нет 18 лет")
-                    return
-                for j in range(1, i.duration + 1):
-                    print(j, end=' ', flush=True)
-                    time.sleep(0.1)
-                print("\nКонец.")
-                return
-
-        print("Видео не найдено.")
+        for video in self.videos:
+            if title == video.title:
+                if video.adult_mode and self.current_user.age >= 18:
+                    while video.time_now < video.duration:
+                        video.time_now += 1
+                        print(video.time_now, end=' ')
+                        time.sleep(1)
+                    video.time_now = 0
+                    print('Конец видео')
+                else:
+                    print('Вам нет 18 лет, пожалуйста покиньте страницу')
+                break
 
 
 ur = UrTube()
